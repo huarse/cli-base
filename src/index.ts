7046d62ca6364a1af6210eac38e00e82fc33478e
moyan <moyan@come-future.com>
2020-08-17 22:55:26 +0800

    fix: hotfix, export command

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 // @author Pluto <huarse@gmail.com>
 // @create 2020/08/17 19:25
 
-export { default as BaseAction } from './classes/action';
+export { default as BaseAction, Command } from './classes/action';
 export * as consts from './consts';
 export * as utils from './utils';
 export * as files from './files';
